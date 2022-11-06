c023e1d3396ebf19ee5082297d5920ebdfdeabbd
topandroid <topandroid@pop-os.localdomain>
2022-11-07 06:03:16 +0700

    upload

---
+++ src/sum.js
@@ -0,0 +1,3 @@
+export const sum = (frist,second)=>{
+    return frist + second;
+}
---
+++ test/sum.test.js
@@ -0,0 +1,17 @@
+import { sum } from "../src/sum";
+
+
+test("test sum function",()=>{
+    const result = sum(1,2);
+    expect(result).toBe(3);
+});
+
+test("test sum function 2",()=>{
+    const result = sum(1,2);
+    expect(result).toBe(3);
+});
+
+test("test sum function 3",()=>{
+    const result = sum(1,2);
+    expect(result).toBe(3);
+});
